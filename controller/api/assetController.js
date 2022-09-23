07e760d9b1ad830be7167f158eb7e719f33c46e2
Partha Paul <pparthapaul11@gmail.com>
2022-09-23 10:35:42 +0530

    asset updatinon

--- controller/api/assetController.js
+++ controller/api/assetController.js
@@ -1,28 +1,29 @@
 const Asset = require("../../models/asset");
-const baseUrl = process.env.BASE_UPLOADS_URL;
+const baseUrl= process.env.BASE_UPLOADS_URL;
 const path = require("path");
 const multer = require("multer");
 const { errorMonitor } = require("events");
 
-exports.getAssetFilesForm = (req, res, next) => {
+exports.getAssetFilesForm= (req, res, next) => {
   let message = req.flash("notification");
 
   return res.render("content/asset_files", {
     pageTitle: "Add files",
     oldInput: {
-      file: "",
-      type: "",
-      name: "",
-      institution: "",
+      file: '',
+      type: '',
+      name: '',
+      institution: '',
     },
     errMessage: message.length > 0 ? message[0] : null,
     errFields: {
-      errFile: "",
-      errType: "",
-      errName: "",
-      errInsti: "",
-    },
-  });
+      errFile:  '',
+      errType: '',
+      errName: '',
+      errInsti: ''
+    }
+  })
+
 };
 
 //uploading files
@@ -32,11 +33,7 @@ const storage = multer.diskStorage({
     const uniqueSuffix = Date.now();
     cb(
       null,
-      file.fieldname +
-        "_" +
-        uniqueSuffix +
-        "_" +
-        file.originalname.replace(/\s/g, "")
+      file.fieldname+"_"+ uniqueSuffix+"_"+file.originalname.replace(/\s/g, "")
     );
   },
 });
@@ -44,51 +41,49 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single("file");
 
 exports.postAssetFiles = async (req, res, next) => {
-  let file, file_name;
+  let file,file_name;
 
   upload(req, res, async function (err) {
-    file = baseUrl + req.file.filename;
+    file = baseUrl+req.file.filename;
     file_name = req.file.originalname;
 
-    if (err) {
+    if(err){
       res.status(500).json({ status: "error", ...err });
     }
 
+
     const type = req.body.type,
-      name = req.body.name,
-      institution = req.body.institution;
+          name = req.body.name,
+          institution = req.body.institution;
 
     // console.log(file_name,type,file);
     const asset = new Asset({
-      file_name: file_name,
+      file_name: file_name ,
       name: name,
       file: file,
       type: type || "",
-      institution: institution || "",
+      institution:  institution|| "",
     });
     try {
       await asset.save();
-      res.status(200).json({
-        status: "success",
-        messege: "file uploaded successfully",
-        name: name,
-        file: file,
-        institution: institution,
-      });
+        res.status(200).json({
+          status: "success",
+          messege: "file uploaded successfully",
+          name: name,
+          file: file,
+          institution: institution,
+        });
     } catch (err) {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
     }
-  });
-};
+  }) 
+}
 
 exports.getAssetList = async (req, res, next) => {
   try {
-    const assets = await Asset.find({
-      type: req.params.type,
-      ...req.query,
-    }).sort({ index: 1 });
+    const assets = await Asset.find({type: req.params.type,...req.query}).sort({index: 1});
 
     res.status(200).json({
       status: "success",
@@ -99,7 +94,7 @@ exports.getAssetList = async (req, res, next) => {
     error.httpStatusCode = 500;
     return next(error);
   }
-};
+}
 
 exports.getEditAssetFilesForm = async (req, res, next) => {
   try {
@@ -113,43 +108,44 @@ exports.getEditAssetFilesForm = async (req, res, next) => {
         errTitle: "",
         errDesc: "",
       },
-      oldInput: {
+      oldInput:{
         name: post.name,
         type: post.type,
         institution: post.institution,
-      },
+      }
     });
   } catch (err) {
     return next(err);
   }
-};
+}
 
 exports.postEditAssetFiles = (req, res, next) => {
-  let file, file_name;
+ let file,file_name; 
   upload(req, res, async function (err) {
-    if (err) {
+
+    if(err){
       res.status(500).json({ status: "error", ...err });
     }
 
     const data = {
       name: req.body.name,
-      type: req.body.type,
       institution: req.body.institution,
     };
 
-    if (req.file) {
-      file = baseUrl + req.file.filename;
+    if(req.file){
+      file = baseUrl+req.file.filename;
       file_name = req.file.originalname;
       data.file = file;
       data.file_name = file_name;
     }
 
-    try {
+    try{
+
       const updatedRes = await Asset.updateOne(
         { _id: req.params.id },
         {
           $set: {
-            ...data,
+           ...data 
           },
         }
       );
@@ -157,11 +153,13 @@ exports.postEditAssetFiles = (req, res, next) => {
         status: "success",
         data: updatedRes,
       });
-    } catch (err) {
+    }
+    catch(err){
       res.status(500).json({
         status: "error",
         data: err,
       });
     }
-  });
-};
+  })
+  
+}
