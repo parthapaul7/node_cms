39efb1bf56cd45858d5566c13ec1e71cad7553a2
Partha Paul <pparthapaul11@gmail.com>
2022-09-25 19:31:13 +0530

    filed handeded

--- controller/post.js
+++ controller/post.js
@@ -10,16 +10,25 @@ exports.getPosts = async (req, res, next) => {
   const sortBy = req.query.sortBy || "author";
 
   try {
-  let postsLength,isAbsBlocked,products
-    if(!req.query.search){
+    let postsLength, isAbsBlocked, products;
+    if (!req.query.search) {
       postsLength = await Post.find({}).countDocuments();
-      isAbsBlocked = await Post.find({ abstractId: "default"});
-      products = await Post.find().sort({[sortBy]: 1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
-    }
-    else{
-      postsLength = await Post.find({$text:{$search:`\"${req.query.search}\"`}}).countDocuments();
-      isAbsBlocked = await Post.find({ abstractId: "default"});
-      products = await Post.find({$text:{$search:`\"${req.query.search}\"`}}).sort({[sortBy]: 1}).skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+      isAbsBlocked = await Post.find({ abstractId: "default" });
+      products = await Post.find()
+        .sort({ [sortBy]: 1 })
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    } else {
+      postsLength = await Post.find({
+        $text: { $search: `\"${req.query.search}\"` },
+      }).countDocuments();
+      isAbsBlocked = await Post.find({ abstractId: "default" });
+      products = await Post.find({
+        $text: { $search: `\"${req.query.search}\"` },
+      })
+        .sort({ [sortBy]: 1 })
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
     }
 
     totalItems = postsLength;
@@ -27,7 +36,8 @@ exports.getPosts = async (req, res, next) => {
     res.render("post/post-list", {
       pageTitle: "Abstracts",
       posts: products,
-      isAbsBlocked: isAbsBlocked.length > 0 ? "Abstract submission blocked": "",
+      isAbsBlocked:
+        isAbsBlocked.length > 0 ? "Abstract submission blocked" : "",
       errMessage: message.length > 0 ? message[0] : null,
       itemsPerPage: ITEMS_PER_PAGE,
       totalItems: totalItems,
@@ -38,7 +48,7 @@ exports.getPosts = async (req, res, next) => {
       previousPage: page - 1,
       lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       search: req.query.search,
-      isSortedByDate: req.query.sortBy === "createdAt"? true: false,
+      isSortedByDate: req.query.sortBy === "createdAt" ? true : false,
     });
   } catch (err) {
     const error = new Error(err);
@@ -51,8 +61,9 @@ exports.getPostDetail = async (req, res, next) => {
   var message = req.flash("notification");
   try {
     const post = (await Post.find({ _id: req.params.postId }))[0];
-    const temp= new Date(post.createdAt);
-    const submissionDate = temp.toLocaleDateString()+" | "+temp.toLocaleTimeString();
+    const temp = new Date(post.createdAt);
+    const submissionDate =
+      temp.toLocaleDateString() + " | " + temp.toLocaleTimeString();
     console.log(submissionDate);
     res.render("post/post-detail", {
       pageTitle: post.title,
@@ -84,14 +95,18 @@ exports.getPostDetail = async (req, res, next) => {
 //   });
 // };
 
-// upload files 
+// upload files
 const storage = multer.diskStorage({
   destination: "abstracts/",
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now();
     cb(
       null,
-      file.fieldname+"_"+ uniqueSuffix+"_"+file.originalname.replace(/\s/g, "")
+      file.fieldname +
+        "_" +
+        uniqueSuffix +
+        "_" +
+        file.originalname.replace(/\s/g, "")
     );
   },
 });
@@ -99,54 +114,58 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single("abstract");
 
 exports.postAddPost = (req, res, next) => {
-
   upload(req, res, async function (err) {
     // console.log(req.file, "file");
-    if(!req.file){
-      return res.status(400).json({ status: "error", message: "File not uploaded" });
+    if (!req.file) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "File not uploaded" });
     }
-    if(req.file.size > 10000000){
-      return res.status(500).json({ status: "error", message: "File size too large" });
+    if (req.file.size > 10000000) {
+      return res
+        .status(500)
+        .json({ status: "error", message: "File size too large" });
     }
 
-    const file_link = process.env.BASE_FILE_URL_NEW + "abstracts/"+req.file.filename;
+    const file_link =
+      process.env.BASE_FILE_URL_NEW + "abstracts/" + req.file.filename;
 
     const post = new Post({
       ...req.body,
       fileName: file_link,
     });
-  
+
     try {
       const result = await post.save();
+      res.json(result);
       if (result) {
         let formattedNumber = result.index.toLocaleString("en-US", {
           minimumIntegerDigits: 5,
           useGrouping: false,
         });
-        const field = result.theme.replace(/[^A-Z]/g, ''); 
+        const field = result.theme.replace(/[^A-Z]/g, "");
         const absId = field + formattedNumber.toString();
 
-        try{
-          const abs = await Post.updateOne({_id:result._id}, {abstractId: absId });
+        try {
+          await Post.updateOne({ _id: result._id }, { abstractId: absId });
+          const response = (await Post.find({ _id: result._id }))[0];
+          res
+            .status(200)
+            .json({ status: "success", message: "Post added", ...response._doc });
+        } catch (error) {
+          res.status(500).json({ status: "error", message: error });
         }
-        catch(err){
-          console.log(err);
-          res.json({ status: "error", message: "Error updating abstract id" });
-        }
-        
-        const response = (await Post.find({ _id: result._id }))[0];
-        res
-          .status(200)
-          .json({ status: "success", message: "Post added", ...response._doc });
+
       }
-      res.status(400).json({ status: "error", message: "something went wrong" });
+      res
+        .status(400)
+        .json({ status: "error", message: "something went wrong" });
     } catch (err) {
       res
         .status(500)
-        .json({ status: "error", message: "Post not added", ...err });
+        .json({ status: "error", message: "Abstract not added", ...err });
     }
   });
-
 };
 
 exports.getEditPost = async (req, res, next) => {
